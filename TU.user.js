b9006bc12ed959ffd12accdd35249cd986643384
Elvin Chen <20195216+ElvinC@users.noreply.github.com>
2022-04-03 01:47:17 +0200

    fix URL's

--- TU.user.js
+++ TU.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         TU Delft/e /r/placea Template
+// @name         TU Delft/e /r/place Template
 // @namespace    http://tampermonkey.net/
 // @version      0.8
 // @description  TU Delft and TU/e Template
 // @author       /u/fiercedude and updated by /u/byteyotta
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/elvinc/rplaceTU/raw/main/TU.user.js
-// @downloadURL  https://github.com/elvinc/rplaceTU/raw/main/TU.user.js
+// @updateURL    https://github.com/ElvinC/rplaceTU/raw/master/TU.user.js
+// @downloadURL  https://github.com/ElvinC/rplaceTU/raw/master/TU.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -30,7 +30,7 @@ if (window.top !== window.self) {
 						const i = document.createElement("img");
 						const cacheCode = Math.floor(Date.now() / 100000);
 						i.src =
-							"https://raw.githubusercontent.com/elvinc/rplacespace/main/TU_template.png?cache=" +
+							"https://raw.githubusercontent.com/ElvinC/rplaceTU/master/TU_template.png?cache=" +
 							cacheCode;
 						i.style =
 							"position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
